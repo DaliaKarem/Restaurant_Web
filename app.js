189625b59538fe6f3bf74bbac066962afa8428f2
Dalia Karem <daliakarem0027@gmail.com>
2023-11-16 12:51:42 +0200

    version1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app=express();
+const path = require('path');
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/',(req,res)=>{
+    //res.sendFile(path.join(__dirname,'start.html'));
+})
+
+app.use((req,res)=>{
+    res.status(404);
+    res.send('<h1>404 Not Found</h1>');
+})
+app.listen(8000,()=>{
+    console.log('server is running on 8000');
+})
