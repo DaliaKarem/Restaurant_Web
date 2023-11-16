0e251ef3d7d9c253bc1770404ba1929ae37cc380
Dalia Karem <daliakarem0027@gmail.com>
2023-11-16 13:13:37 +0200

    Version1

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app=express();
 const path = require('path');
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/',(req,res)=>{
-    //res.sendFile(path.join(__dirname,'start.html'));
-})
+
 
 app.use((req,res)=>{
     res.status(404);
